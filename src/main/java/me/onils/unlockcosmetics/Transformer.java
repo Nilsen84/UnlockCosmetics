388265db307907d074f29511993e222be3212a0e
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-11-30 22:25:57 +0100

    Initial commit

---
+++ src/main/java/me/onils/unlockcosmetics/Transformer.java
@@ -0,0 +1,25 @@
+package me.onils.unlockcosmetics;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Transformer implements ClassFileTransformer {
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        if (classfileBuffer == null || classfileBuffer.length == 0) {
+            return new byte[0];
+        }
+
+        return classfileBuffer;
+    }
+}
