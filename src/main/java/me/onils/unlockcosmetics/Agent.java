e490b2133ec4cdf024dee2254cc01c9c5478265a
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-12-30 23:36:45 +0100

    Added a websocket server

--- src/main/java/me/onils/unlockcosmetics/Agent.java
+++ src/main/java/me/onils/unlockcosmetics/Agent.java
@@ -1,9 +1,14 @@
 package me.onils.unlockcosmetics;
 
+import me.onils.unlockcosmetics.websocket.ProxyServer;
+
 import java.lang.instrument.Instrumentation;
 
 public class Agent {
     public static void premain(String args, Instrumentation inst){
+        ProxyServer proxyServer = new ProxyServer(1234);
+        proxyServer.start();
+
         inst.addTransformer(new WebsocketTransformer());
     }
 }
