b492100376d27078d957ba41ca071501f15ee6ae
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-12-31 17:48:01 +0100

    Added a new packet to prevent illegal cosmetics from being synced with lunars websocket

--- src/main/java/me/onils/unlockcosmetics/proxy/packet/WSPacket.java
+++ src/main/java/me/onils/unlockcosmetics/proxy/packet/WSPacket.java
@@ -1,6 +1,8 @@
 package me.onils.unlockcosmetics.proxy.packet;
 
 import io.netty.buffer.ByteBuf;
+import me.onils.unlockcosmetics.proxy.Proxy;
+import me.onils.unlockcosmetics.proxy.packet.impl.WSPacketCosmeticEquip;
 import me.onils.unlockcosmetics.proxy.packet.impl.WSPacketCosmeticGive;
 import me.onils.unlockcosmetics.util.PacketBuffer;
 
@@ -8,15 +10,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class WSPacket {
-    public static Map<Integer, Class<? extends WSPacket>> REGISTRY;
+    public static final Map<Integer, Class<? extends WSPacket>> REGISTRY;
 
     public abstract void write(PacketBuffer buffer);
     public abstract void read(PacketBuffer buffer);
-    public abstract void process();
+    public abstract void process(Proxy proxy);
 
     static {
         REGISTRY = new HashMap<>();
 
         REGISTRY.put(8, WSPacketCosmeticGive.class);
+        REGISTRY.put(20, WSPacketCosmeticEquip.class);
     }
 }
---
+++ src/main/java/me/onils/unlockcosmetics/proxy/packet/impl/WSPacketCosmeticEquip.java
@@ -0,0 +1,36 @@
+package me.onils.unlockcosmetics.proxy.packet.impl;
+
+import me.onils.unlockcosmetics.proxy.Proxy;
+import me.onils.unlockcosmetics.proxy.packet.WSPacket;
+import me.onils.unlockcosmetics.util.PacketBuffer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WSPacketCosmeticEquip extends WSPacket {
+    public Map<Long, Boolean> cosmetics;
+
+    @Override
+    public void write(PacketBuffer buffer) {
+        buffer.writeInt(cosmetics.size());
+
+        for(Map.Entry<Long, Boolean> entry : cosmetics.entrySet()){
+            buffer.writeLong(entry.getKey());
+            buffer.writeBoolean(entry.getValue());
+        }
+    }
+
+    @Override
+    public void read(PacketBuffer buffer) {
+        this.cosmetics = new HashMap<>();
+        int size = buffer.readInt();
+        for(int i = 0; i < size; ++i){
+            cosmetics.put(buffer.readLong(), buffer.readBoolean());
+        }
+    }
+
+    @Override
+    public void process(Proxy proxy) {
+        cosmetics.keySet().removeIf(id -> !proxy.getPurchasedCosmetics().contains(id.intValue()));
+    }
+}
